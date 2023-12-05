e61967de8ea2547fca46081236072c8f5cb6ad69
Milanpraz <mln.praz12@gmail.com>
2023-12-05 23:01:54 +0545

    updated

--- src/pages/CreateProperty.jsx
+++ src/pages/CreateProperty.jsx
@@ -391,7 +391,7 @@ function CreateProperty() {
                   htmlFor="image"
                   className="block text-sm  font-medium leading-6 text-gray-900"
                 >
-                  Add Profile Picture
+                  Add Property Picture
                 </label>
                 <div className="mt-2">
                   <input
